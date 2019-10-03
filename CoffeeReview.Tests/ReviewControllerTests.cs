18bec1c0f8c37d3afd577e484f139444f05b193b
Andy Kohler <andykohler99@gmail.com>
2019-10-03 15:27:08 -0400

    MVC Items, passed one test and failed one

---
+++ CoffeeReview.Tests/ReviewControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xunit;
+using CoffeeReview.Tests;
+using CoffeeReview.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using CoffeeReview.Models;
+
+namespace CoffeeReview.Tests
+{
+    public class ReviewControllerTests
+    {
+        [Fact]
+        public void Review_Returns_ViewResult()
+        {
+            var controller = new ReviewController();
+
+            var result = controller.Index();
+
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void Review_Passes_ReviewModels_To_View()
+        {
+            var controller = new ReviewController();
+
+            var result = controller.Index();
+
+            Assert.IsType<ReviewModels>(result.Model);
+        }
+    }
+}
