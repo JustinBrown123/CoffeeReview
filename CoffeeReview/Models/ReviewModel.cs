3a78b1e891ae2f63444533579e024d63b89c782b
Andy Kohler <andykohler99@gmail.com>
2019-10-04 17:11:34 -0400

    set up constructor in ReviewModel; made ReviewRepository

--- CoffeeReview/Models/ReviewModel.cs
+++ CoffeeReview/Models/ReviewModel.cs
@@ -7,13 +7,35 @@ namespace CoffeeReview.Models
 {
     public class ReviewModels
     {
-        public ReviewModels
-        
+        public int Id { get; set; }
 
+        public string Title { get; set; }
 
+        public string ImageUrl { get; set; }
 
+        public string ReviewCategory { get; set; }
 
+        public string Content { get; set; }
 
+        public ReviewModels()
+        {
 
+        }
+
+        public ReviewModels(int Id, string Title, string ImageUrl, string ReviewCategory, string Content, string ReviewModels)
+        {
+        Id = id;
+
+        Title = title;
+
+        ImageUrl = imageUrl;
+
+        ReviewCategory = reviewCategory;
+
+        Content = content;
+
+        ReviewModels = reviewModels;
+
+        }
     }
 }
