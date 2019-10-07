8f736b34571be59086091d039a9576b590b28f8a
James Groves <triskept@gmail.com>
2019-10-07 11:31:23 -0400

    refactor dictionary

--- CoffeeReview/Repositories/CoffeeRepository.cs
+++ CoffeeReview/Repositories/CoffeeRepository.cs
@@ -18,18 +18,22 @@ namespace CoffeeReview.Repositories
 
         public CoffeeRepository()
         {
-            Dictionary<int, Coffee> coffeeDictionary = new Dictionary<int, Coffee>();
-
-            coffeeDictionary.Add(1, new Coffee(1, "Cleveland Coffee", "Bright Side Blend", "happyhappy stuff"));
-            coffeeDictionary.Add(2, new Coffee(2, "Cleveland Coffee", "Tanzanian Peaberry", "really good stuff"));
-            coffeeDictionary.Add(3, new Coffee(3, "Rising Star", "Apo Cooperative", "makes happy"));
-            coffeeDictionary.Add(4, new Coffee(4, "Starbucks", "Komodo Dragon", "happyhappy stuff"));
-            coffeeDictionary.Add(5, new Coffee(5, "8 O'Clock", "The Original", "great cheap coffee"));
-            coffeeDictionary.Add(6, new Coffee(6, "Brioso", "Bright Side Blend", "Pretty Solid"));
-            coffeeDictionary.Add(7, new Coffee(7, "8 O'Clock", "Red Eye Roast", "excellent for the cost"));
-            coffeeDictionary.Add(8, new Coffee(8, "Peets", "Costa Rica Peaberry", "Deliciousness"));
-            coffeeDictionary.Add(9, new Coffee(9, "Dunkin' Donuts", "Dunkin Decaf", "Horrible"));
+            coffeeDictionary = new Dictionary<int, Coffee>()
+            {
+                { 1, new Coffee(1, "Cleveland Coffee", "Bright Side Blend", "happyhappy stuff")},
+                { 2, new Coffee(2, "Cleveland Coffee", "Tanzanian Peaberry", "really good stuff")},
+                { 3, new Coffee(3, "Rising Star", "Apo Cooperative", "makes happy")},
+                { 4, new Coffee(4, "Starbucks", "Komodo Dragon", "happyhappy stuff")},
+                { 5, new Coffee(5, "8 O'Clock", "The Original", "great cheap coffee")},
+                { 6, new Coffee(6, "Brioso", "Bright Side Blend", "Pretty Solid")},
+                { 7, new Coffee(7, "8 O'Clock", "Red Eye Roast", "excellent for the cost")},
+                { 8, new Coffee(8, "Peets", "Costa Rica Peaberry", "Deliciousness")},
+                { 9, new Coffee(9, "Dunkin' Donuts", "Dunkin Decaf", "Horrible")},
+            };
         }
+
+      
+        
          
         public IEnumerable<Coffee> GetAll()
         {
