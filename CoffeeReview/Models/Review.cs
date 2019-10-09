60e3178a7d293b70a04bc90d72c18430b883c210
James Groves <triskept@gmail.com>
2019-10-09 10:51:58 -0400

    coffeecontext

--- CoffeeReview/Models/Review.cs
+++ CoffeeReview/Models/Review.cs
@@ -7,5 +7,19 @@ namespace CoffeeReview.Models
 {
     public class Review
     {
+        public int Id { get; set; }
+        public string Content { get; set; }
+
+        public Coffee Coffee { get; set; }
+
+        public Review(int id, string content)
+        {
+            Id = id;
+            Content = content;
+        }
+
+        public Review()
+        {
+        }
     }
 }
