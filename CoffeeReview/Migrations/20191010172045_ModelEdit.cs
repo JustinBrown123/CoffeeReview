5aa042758344657d362e4825f8bd3e627ab18a18
Your Name <you@example.com>
2019-10-10 14:08:48 -0400

    migrations

---
+++ CoffeeReview/Migrations/20191010172045_ModelEdit.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CoffeeReview.Migrations
+{
+    public partial class ModelEdit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Review_Coffees_CoffeeId",
+                table: "Review");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Coffees",
+                table: "Coffees");
+
+            migrationBuilder.RenameTable(
+                name: "Coffees",
+                newName: "Coffee");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Coffee",
+                table: "Coffee",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Review_Coffee_CoffeeId",
+                table: "Review",
+                column: "CoffeeId",
+                principalTable: "Coffee",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Review_Coffee_CoffeeId",
+                table: "Review");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Coffee",
+                table: "Coffee");
+
+            migrationBuilder.RenameTable(
+                name: "Coffee",
+                newName: "Coffees");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Coffees",
+                table: "Coffees",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Review_Coffees_CoffeeId",
+                table: "Review",
+                column: "CoffeeId",
+                principalTable: "Coffees",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
