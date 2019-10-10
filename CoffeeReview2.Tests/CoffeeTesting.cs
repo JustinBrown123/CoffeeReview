a1aa77d7e7f230746ffa6e0e75776991cfcccca9
Andy Kohler <andykohler99@gmail.com>
2019-10-10 13:49:12 -0400

    Uncommented Coffee and Review Tests

--- CoffeeReview2.Tests/CoffeeTesting.cs
+++ CoffeeReview2.Tests/CoffeeTesting.cs
@@ -1,78 +1,154 @@
+using CoffeeReview;
+using CoffeeReview.Models;
+using CoffeeReview.Repositories;
 using System;
+using System.Linq;
 using Xunit;
 
 namespace CoffeeReview2.Tests
 {
-    //public class SongRepositoryTests : IDisposable
-    //{
-    //    private MusicContext db;
-    //    private SongRepository underTest;
+    public class CoffeeRepositoryTests : IDisposable
+    {
+        private CoffeeContext db;
+        private CoffeeRepository underTest;
 
-    //    public SongRepositoryTests()
-    //    {
-    //        db = new MusicContext();
-    //        db.Database.BeginTransaction();
+        public CoffeeRepositoryTests()
+        {
+            db = new CoffeeContext();
+            db.Database.BeginTransaction();
 
-    //        underTest = new SongRepository(db);
-    //    }
+            underTest = new CoffeeRepository(db);
+        }
 
-    //    public void Dispose()
-    //    {
-    //        db.Database.RollbackTransaction();
-    //    }
+        public void Dispose()
+        {
+            db.Database.RollbackTransaction();
+        }
 
-    //    [Fact]
-    //    public void Count_Starts_At_Zero()
-    //    {
-    //        var count = underTest.Count();
+        [Fact]
+        public void Count_Starts_At_Zero()
+        {
+            var count = underTest.Count();
 
-    //        Assert.Equal(0, count);
-    //    }
+            Assert.Equal(0, count);
+        }
 
-    //    [Fact]
-    //    public void Create_Increases_Count()
-    //    {
-    //        underTest.Create(new Song() { Title = "Foo" });
+        [Fact]
+        public void Create_Increases_Count()
+        {
+            underTest.Create(new Coffee() { Brand = "Foo" });
 
-    //        var count = underTest.Count();
-    //        Assert.Equal(1, count);
-    //    }
+            var count = underTest.Count();
+            Assert.Equal(1, count);
+        }
 
-    //    [Fact]
-    //    public void GetById_Returns_Created_Item()
-    //    {
-    //        var expectedSong = new Song() { Title = "Baby Shark" };
-    //        underTest.Create(expectedSong);
+        [Fact]
+        public void GetById_Returns_Created_Item()
+        {
+            var expectedSong = new Coffee() { Brand = "Baby Coffee" };
+            underTest.Create(expectedSong);
 
-    //        var result = underTest.GetById(expectedSong.Id);  // The Id was set by EF when we call Create above.
+            var result = underTest.GetById(expectedSong.Id);  // The Id was set by EF when we call Create above.
 
-    //        Assert.Equal(expectedSong.Title, result.Title);
-    //    }
+            Assert.Equal(expectedSong.Brand, result.Brand);
+        }
 
-    //    [Fact]
-    //    public void Delete_Reduces_Count()
-    //    {
-    //        var song = new Song() { Title = "Baby Shark" };
-    //        underTest.Create(song);
+        [Fact]
+        public void Delete_Reduces_Count()
+        {
+            var coffee = new Coffee() { Brand = "Baby Coffee" };
+            underTest.Create(coffee);
 
-    //        underTest.Delete(song);
-    //        var count = underTest.Count();
+            underTest.Delete(coffee);
+            var count = underTest.Count();
 
-    //        Assert.Equal(0, count);
-    //    }
+            Assert.Equal(0, count);
+        }
 
-    //    [Fact]
-    //    public void GetAll_Returns_All()
-    //    {
-    //        underTest.Create(new Song() { Title = "Baby Shark" });
-    //        underTest.Create(new Song() { Title = "Never gonna give you up" });
+        [Fact]
+        public void GetAll_Returns_All()
+        {
+            underTest.Create(new Coffee() { Brand = "Baby Coffee" });
+            underTest.Create(new Coffee() { Brand = "Never gonna give you up" });
 
-    //        var all = underTest.GetAll();
+            var all = underTest.GetAll();
 
-    //        Assert.Equal(2, all.Count());
-    //    }
+            Assert.Equal(2, all.Count());
+        }
 
-    // Save or Update?
-    //}
+        //Save or Update?
+    }
+
+    public class ReviewRepositoryTests : IDisposable
+    {
+        private CoffeeContext db;
+        private ReviewRepository underTest;
+
+        public ReviewRepositoryTests()
+        {
+            db = new CoffeeContext();
+            db.Database.BeginTransaction();
+
+            underTest = new ReviewRepository(db);
+        }
+
+        public void Dispose()
+        {
+            db.Database.RollbackTransaction();
+        }
+
+        [Fact]
+        public void Count_Starts_At_Zero()
+        {
+            var count = underTest.Count();
+
+            Assert.Equal(0, count);
+        }
+
+        [Fact]
+        public void Create_Increases_Count()
+        {
+            underTest.Create(new Review() { Coffee = "Foo" });
+
+            var count = underTest.Count();
+            Assert.Equal(1, count);
+        }
+
+        [Fact]
+        public void GetById_Returns_Created_Item()
+        {
+            var expectedReview = new Review() { Coffee = "Baby Review" };
+            underTest.Create(expectedReview);
+
+            var result = underTest.GetById(expectedCoffee.Id);  // The Id was set by EF when we call Create above.
+
+            Assert.Equal(expectedReview.Coffee, result.Coffee);
+        }
+
+        [Fact]
+        public void Delete_Reduces_Count()
+        {
+            var review = new Review() { Coffee = "Baby Review" };
+            underTest.Create(review);
+
+            underTest.Delete(review);
+            var count = underTest.Count();
+
+            Assert.Equal(0, count);
+        }
+
+        [Fact]
+        public void GetAll_Returns_All()
+        {
+            underTest.Create(new Review() { Coffee = "Baby Review" });
+            underTest.Create(new Review() { Coffee = "Never gonna give you up" });
+
+            var all = underTest.GetAll();
+
+            Assert.Equal(2, all.Count());
+        }
+
+        //Save or Update?
+    }
 
 }
