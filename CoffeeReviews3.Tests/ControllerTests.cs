93f08a542589e6695ca247497081ba79720074e2
Andy Kohler <andykohler99@gmail.com>
2019-10-15 15:22:18 -0400

    Added NUnit Tests

---
+++ CoffeeReviews3.Tests/ControllerTests.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using NSubstitute;
+
+namespace ControllerTests
+{
+    public class ControllerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
