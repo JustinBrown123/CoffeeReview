a91cf4a7c7340a9232435642680ae4175e7bde41
Andy Kohler <andykohler99@gmail.com>
2019-10-15 15:35:35 -0400

    Added NUnit Tests

--- CoffeeReviews3.Tests/ControllerTests.cs
+++ CoffeeReviews3.Tests/ControllerTests.cs
@@ -13,6 +13,11 @@ namespace ControllerTests
         [Test]
         public void Test1()
         {
+            ReviewRepo = Substitute.For<IRepository<Review>>();
+           
+            underTest = new ReviewController(ReviewRepo);
+
+            ReviewRepo.GetAll().Returns(expectedReviews);
             Assert.Pass();
         }
     }
