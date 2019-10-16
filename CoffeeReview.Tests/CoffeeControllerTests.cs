41fc289727e6c6fd1b34eef7965fee892c43d921
Andy Kohler <andykohler99@gmail.com>
2019-10-16 15:27:41 -0400

    Tests Fixed

--- CoffeeReview.Tests/CoffeeControllerTests.cs
+++ CoffeeReview.Tests/CoffeeControllerTests.cs
@@ -1,6 +1,7 @@
 ﻿using CoffeeReview.Controllers;
 using CoffeeReview.Models;
 using CoffeeReview.Repositories;
+using Microsoft.AspNetCore.Mvc;
 using NSubstitute;
 using System;
 using System.Collections.Generic;
@@ -11,15 +12,52 @@ namespace CoffeeReview.Tests
 {
     public class CoffeeControllerTests
     {
+        CoffeeController underTest;
+        IRepository<Coffee> coffeeRepo;
+
+        public CoffeeControllerTests()
+        {
+            coffeeRepo = Substitute.For<IRepository<Coffee>>();
+            underTest = new CoffeeController(coffeeRepo);
+        }
+        
+        [Fact]
+        public void Index_Returns_A_View()
+        {
+            var result = underTest.Index();
+
+            Assert.IsType<ViewResult>(result);
+        }
 
         [Fact]
-        public void GetAll_Returns_All_Reviews()
+        public void Index_Passes_All_Coffees_To_View()
         {
-            CoffeeRepository = Substitute.For<IRepository<Coffee>>();
+            var expectedCoffees = new List<Coffee>();
+            coffeeRepo.GetAll().Returns(expectedCoffees);
 
-            underTest = new CoffeeController(CoffeeRepository);
+            var result = underTest.Index();
 
-            CoffeeRepository.GetAll().Returns(expectedCoffees);
+            Assert.Equal(expectedCoffees, result.Model);
         }
+
+        [Fact]
+        public void Details_Returns_A_View()
+        {
+            var result = underTest.Details(1);
+
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void Details_Passes_Course_To_View()
+        {
+            var expectedCoffee = new Coffee();
+            coffeeRepo.GetById(1).Returns(expectedCoffee);
+
+            var result = underTest.Details(1);
+
+            Assert.Equal(expectedCoffee, result.Model);
+        }
+
     }
 }
